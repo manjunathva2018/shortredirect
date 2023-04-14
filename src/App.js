9962b48a4da57b3d6574157e749610cb5ae7ddda
manjunath <manjunathtestproject@gmail.com>
2023-04-14 09:42:20 +0530

    inital

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+export default function External() {
+  window.location.href = 'https://open.spotify.com/artist/12gRaSBN8D900VCHXHmVSy';
+  return null;
+}
